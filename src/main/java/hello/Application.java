ca757a214123623b10c18b8bc435224c79947bc6
srevereault <sylvain.revereault@gmail.com>
2017-05-03 10:37:22 +0200

    appli gradle avec paramétrage de version

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Application lancée");
+    }
+
+}
